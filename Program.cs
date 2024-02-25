88d592ee50c74d767c3b9ad33fe09d561a205db8
Denis <den.grigorev.2002@list.ru>
2024-02-25 19:23:34 +0300

    Write function FormatString

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿string[] FormatString (string[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i].Length <= 3){
+            count++;
+        }
+    }
+    string[] arr = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i].Length <= 3){
+            arr[j] = array[i];
+            j++;
+        }
+    }
+    
+    return arr;
+}
